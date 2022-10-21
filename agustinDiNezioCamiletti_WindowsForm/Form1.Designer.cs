a7a914df6bf3b9fc0adfdc062e7d80dae874df09
Agustin Di Nezio <adineziocamiletti@gmail.com>
2022-10-21 15:33:03 -0300

    Agregar archivos de proyecto.

---
+++ agustinDiNezioCamiletti_WindowsForm/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace agustinDiNezioCamiletti_WindowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblSueldo = new System.Windows.Forms.Label();
+            this.lblPuesto = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtSueldo = new System.Windows.Forms.TextBox();
+            this.txtPuesto = new System.Windows.Forms.TextBox();
+            this.btnValidaciones = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnHoras = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(251, 68);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(50, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Nombre :";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(251, 104);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(50, 13);
+            this.lblApellido.TabIndex = 1;
+            this.lblApellido.Text = "Apellido :";
+            // 
+            // lblSueldo
+            // 
+            this.lblSueldo.AutoSize = true;
+            this.lblSueldo.Location = new System.Drawing.Point(251, 145);
+            this.lblSueldo.Name = "lblSueldo";
+            this.lblSueldo.Size = new System.Drawing.Size(46, 13);
+            this.lblSueldo.TabIndex = 2;
+            this.lblSueldo.Text = "Sueldo :";
+            // 
+            // lblPuesto
+            // 
+            this.lblPuesto.AutoSize = true;
+            this.lblPuesto.Location = new System.Drawing.Point(251, 186);
+            this.lblPuesto.Name = "lblPuesto";
+            this.lblPuesto.Size = new System.Drawing.Size(46, 13);
+            this.lblPuesto.TabIndex = 3;
+            this.lblPuesto.Text = "Puesto :";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(342, 68);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(342, 101);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 5;
+            // 
+            // txtSueldo
+            // 
+            this.txtSueldo.Location = new System.Drawing.Point(342, 142);
+            this.txtSueldo.Name = "txtSueldo";
+            this.txtSueldo.Size = new System.Drawing.Size(100, 20);
+            this.txtSueldo.TabIndex = 6;
+            // 
+            // txtPuesto
+            // 
+            this.txtPuesto.Location = new System.Drawing.Point(342, 183);
+            this.txtPuesto.Name = "txtPuesto";
+            this.txtPuesto.Size = new System.Drawing.Size(100, 20);
+            this.txtPuesto.TabIndex = 7;
+            // 
+            // btnValidaciones
+            // 
+            this.btnValidaciones.Location = new System.Drawing.Point(524, 64);
+            this.btnValidaciones.Name = "btnValidaciones";
+            this.btnValidaciones.Size = new System.Drawing.Size(112, 23);
+            this.btnValidaciones.TabIndex = 8;
+            this.btnValidaciones.Text = "Validaciones";
+            this.btnValidaciones.UseVisualStyleBackColor = true;
+            this.btnValidaciones.Click += new System.EventHandler(this.btnValidaciones_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(524, 101);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(112, 23);
+            this.btnMostrar.TabIndex = 9;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // btnHoras
+            // 
+            this.btnHoras.Location = new System.Drawing.Point(524, 142);
+            this.btnHoras.Name = "btnHoras";
+            this.btnHoras.Size = new System.Drawing.Size(112, 23);
+            this.btnHoras.TabIndex = 10;
+            this.btnHoras.Text = "Ingresar Horas";
+            this.btnHoras.UseVisualStyleBackColor = true;
+            this.btnHoras.Click += new System.EventHandler(this.btnHoras_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(524, 183);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(112, 23);
+            this.btnLimpiar.TabIndex = 11;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnHoras);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnValidaciones);
+            this.Controls.Add(this.txtPuesto);
+            this.Controls.Add(this.txtSueldo);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblPuesto);
+            this.Controls.Add(this.lblSueldo);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblSueldo;
+        private System.Windows.Forms.Label lblPuesto;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtSueldo;
+        private System.Windows.Forms.TextBox txtPuesto;
+        private System.Windows.Forms.Button btnValidaciones;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnHoras;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ agustinDiNezioCamiletti_WindowsForm/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace agustinDiNezioCamiletti_WindowsForm
+{
+    public partial class Form1 : Form
+    {
+        string nombre="";
+        string apellido="";
+        double sueldo=0;
+        string puesto="";
+        int[] horas;
+        string[] dias = new string[5] { "Lunes", "Martes", "Miercoles", "Jueves", "Viernes" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidaciones_Click(object sender, EventArgs e)
+        {
+            nombre = txtName.Text.ToUpper();
+            apellido = txtApellido.Text.ToUpper();
+            sueldo = Convert.ToDouble(txtSueldo.Text);
+            puesto = txtPuesto.Text.ToUpper();
+
+            if (ValidaNombre(nombre)==0) MessageBox.Show("Nombre invalido, intente nuevamente.");
+            if (ValidaApellido(apellido) == 0) MessageBox.Show("Apellido invalido, intente nuevamente");
+            if (ValidarSueldo(sueldo) == 0) MessageBox.Show("Sueldo incorrecto, intente nuevamente.");
+            if (ValidarPuesto(puesto)==0) MessageBox.Show("Puesto no existente, intente nuevamente.");
+            
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            MostrarInfo(nombre, apellido, puesto);
+        }
+
+        private void btnHoras_Click(object sender, EventArgs e)
+        {
+            horas = new int[5];
+            int horasTotales = 0;
+            double promedioHoras;
+            int menorHoras;
+            CargaHoras(horas);
+            horasTotales=TotalHoras(horas);
+            promedioHoras = PromedioHoras(horas);
+            menorHoras = DiaMenosHoras(horas);
+
+            MessageBox.Show($"El total de horas trabajadas es : {horasTotales}");
+            MessageBox.Show($"El promedio de horas trabajadas es : {promedioHoras}");
+            MessageBox.Show($"El dia de menor horas trabajadas es : {dias[menorHoras]}");
+
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            LimpiarTxt();
+        }
+
+        //-----------------------------------------------------------------------------------------------
+        //-----------------------------------------METODOS-----------------------------------------------
+        //-----------------------------------------------------------------------------------------------
+
+        private int ValidarPuesto(string puesto) // Valida el Puesto
+        {
+            int flag = 0;
+            switch (puesto)
+            {
+                case "SOPORTE TECNICO":
+                case "DBA":
+                case "DESARROLLADOR":
+                    flag += 1;
+                    return flag;
+            }
+            return flag;
+        }
+        private int ValidarSueldo(double sueldo) //Valida el sueldo
+        {
+            int flag = 0;
+            if (sueldo > 0) flag = 1;
+            return flag;
+        }
+
+        private void MostrarInfo(string nombre, string apellido, string puesto)  //Muestra Nombre, Apellido y Puesto
+        {
+            MessageBox.Show($"Nombre : {nombre}\nApellido : {apellido}\nPuesto : {puesto}");
+        }
+
+        private void CargaHoras(int[] horas)    //Carga horas trabajadas
+        {
+            for (int i = 0; i < horas.Length; i++)
+            {
+                horas[i] = Convert.ToInt32(Interaction.InputBox($"{i + 1}. Ingresa horas trabajadas","Horas Diarias"));
+            }
+        }
+
+        private int TotalHoras(int[] horas) // Calcula el total de horas trabajadas.
+        {
+            int total = 0;
+            for (int i = 0; i < horas.Length; i++)
+            {
+                total += horas[i];
+            }
+            return total;
+        }
+        
+        private void LimpiarTxt() // Limpia contenido de los textBox
+        {
+            txtName.Clear();
+            txtApellido.Clear();
+            txtSueldo.Clear();
+            txtPuesto.Clear();
+        }
+
+        private int ValidaNombre(string nombre) //Valida Nombre
+        {
+            int flag = 0;
+            if (nombre.Length > 2 && nombre.Length < 50) flag=1;
+            return flag;
+        }
+
+        private int ValidaApellido(string apellido) //Valida Apellido
+        {
+            int flag = 0;
+            if (apellido.Length > 2 && nombre.Length < 50) flag = 1;
+            return flag;
+        }
+
+        private double PromedioHoras(int[] horas) // Calcula Promedio horas trabajadas
+        {
+            double suma = 0;
+            for (int i = 0; i < horas.Length; i++)
+            {
+                suma += horas[i];
+            }
+            return suma/horas.Count();
+        }
+
+        private int DiaMenosHoras(int[] horas)
+        {
+            int pos = 0;
+            int menor = 99;
+            for (int i = 0; i < horas.Length; i++)
+            {
+                if (horas[i] < menor)
+                {
+                    menor = horas[i];
+                    pos = i;
+                }
+            }
+            return pos;
+        }
+    }
+}
